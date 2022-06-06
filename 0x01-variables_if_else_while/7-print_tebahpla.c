22e59a19b19429283283ac04dc5e9fff9792e6b2
Obokparo Ejemuta <henry.ejemuta@gmail.com>
2022-06-05 22:28:46 -0700

    Added a C program that prints the lowercase alphabet in reverse, followed by a new line.
    You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
    All your code should be in the main function
    You can only use putchar twice in your code

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * A C program that prints the lowercase alphabet in reverse,
+ * followed by a new line.
+ *
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * All your code should be in the main function
+ * You can only use putchar twice in your code
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int cha;
+
+	for (cha = 'z'; cha >= 'a'; cha--)
+	{
+		putchar(cha);
+	}
+	putchar('\n');
+	return (0);
+}
