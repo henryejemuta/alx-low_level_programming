939d5aa67b05e698937fc927add1ac65719e660c
Obokparo Ejemuta <henry.ejemuta@gmail.com>
2022-06-05 23:03:41 -0700

    Added a C  program that prints all possible different combinations of three digits.
    Numbers must be separated by ,, followed by a space
    The three digits must be different
    012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1 and 2
    Print only the smallest combination of three digits
    Numbers should be printed in ascending order, with three digits
    You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
    You can only use putchar six times maximum in your code
    You are not allowed to use any variable of type char
    All your code should be in the main function

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * A C program that prints all possible different
+ * combinations of two digits.
+ *
+ * Numbers must be separated by ,, followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination
+ * of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * Numbers should be printed in ascending order,
+ * with two digits
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int h, i, j;
+
+for (h = 0;  h < 8; h++)
+{
+	for (i = 1; i < 9; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			if (i < j)
+			{
+				putchar(h + '0');
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i !=  8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
