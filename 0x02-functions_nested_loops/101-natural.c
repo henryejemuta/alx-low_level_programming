fc78a5d91a9bcba15a6d930f0e2815b1a01ef179
Obokparo Ejemuta <henry.ejemuta@gmail.com>
2022-06-09 21:43:10 -0700

    Fix 101 natural to use custom print

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,47 @@
 #include <stdio.h>
+#include "main.h"
 
+
+/**
+ * custom_print - Print without libraries
+ * @n: number argument
+ * Return: Void returner
+ */
+void custom_print(int n)
+{
+	int count = 0;
+	int number = n;
+	int loop, k, j;
+
+	do {
+		n /= 10;
+		++count;
+	} while (n != 0);
+
+	if (count <= 1)
+	{
+		_putchar(number + '0');
+	}
+	else
+	{
+			loop = 10;
+		for (k = 2; k < count; k++)
+		{
+			loop *= 10;
+		}
+		for (j = (count - 1); j >= 0; j--)
+		{
+			int temp_number;
+
+			temp_number = number / loop;
+			if (temp_number != 0)
+			{
+				_putchar((temp_number % 10) + '0');
+				loop /= 10;
+			}
+		}
+	}
+}
 
 /**
  * main - Entry point
@@ -21,7 +63,8 @@ int main(void)
 		}
 
 	}
-	printf("%d\n", sum);
+	custom_print(sum);
+	_putchar('\n');
 
 	return (sum);
 }
