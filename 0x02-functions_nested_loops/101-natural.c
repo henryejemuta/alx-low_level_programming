d1f3f4eef0f2e65d548e3484ad11f3ac0a71753c
Obokparo Ejemuta <henry.ejemuta@gmail.com>
2022-06-09 21:48:26 -0700

    Fix 101 natural by returning 0 instead of the sum

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,47 +3,6 @@
 
 
 /**
- * custom_print - Print without libraries
- * @n: number argument
- * Return: Void returner
- */
-void custom_print(int n)
-{
-	int count = 0;
-	int number = n;
-	int loop, k, j;
-
-	do {
-		n /= 10;
-		++count;
-	} while (n != 0);
-
-	if (count <= 1)
-	{
-		_putchar(number + '0');
-	}
-	else
-	{
-			loop = 10;
-		for (k = 2; k < count; k++)
-		{
-			loop *= 10;
-		}
-		for (j = (count - 1); j >= 0; j--)
-		{
-			int temp_number;
-
-			temp_number = number / loop;
-			if (temp_number != 0)
-			{
-				_putchar((temp_number % 10) + '0');
-				loop /= 10;
-			}
-		}
-	}
-}
-
-/**
  * main - Entry point
  * A C program that computes and prints the sum
  * of all the multiples of 3 or 5 below 1024 (excluded),
@@ -63,8 +22,8 @@ int main(void)
 		}
 
 	}
-	custom_print(sum);
-	_putchar('\n');
 
-	return (sum);
+	printf("%d\n", sum);
+
+	return (0);
 }
