ebd025f3448fbd26af93bd8de48633424cfb2e73
Obokparo Ejemuta <henry.ejemuta@gmail.com>
2022-06-10 22:16:36 -0700

    Updated program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,36 +13,39 @@
  */
 int main(void)
 {
-        int i;
-        unsigned long sum, first, last, f, fr, oldf, oldfr, l, lr;
+int i;
+unsigned long sum, first, last, f, fr, oldf, oldfr, l, lr;
 
-        first = 1;
-        last = 2;
+first = 1;
+last = 2;
 
-        printf("1, 2");
-        for (i = 2; i < 90; i++)
-        {
-                sum = first + last;
-                first = last;
-                last = sum;
-                printf(", %ld", sum);
-        }
+printf("1, 2");
+for (i = 2; i < 90; i++)
+{
+	sum = first + last;
+	first = last;
+	last = sum;
+	printf(", %ld", sum);
+}
 
-        f = first / 1000000000000;
-        fr = first % 1000000000000;
-        l = last / 1000000000000;
-        lr = last % 1000000000000;
-        for (i = 90; i < 98; i++)
-        {
-                oldf = f;
-                oldfr = fr;
-                printf(", %ld", (((oldf * 1000000000000) + oldfr) + ((l * 1000000000000) + lr)));
-                f = l;
-                fr = lr;
-                l = (((oldf * 1000000000000) + oldfr) + ((l * 1000000000000) + lr)) / 1000000000000;
-                lr = (((oldf * 1000000000000) + oldfr) + ((l * 1000000000000) + lr)) % 1000000000000;
-        }
+f = first / 1000000000000;
+fr = first % 1000000000000;
+l = last / 1000000000000;
+lr = last % 1000000000000;
+for (i = 90; i < 98; i++)
+{
+oldf = f;
+oldfr = fr;
+printf(", %s", (((oldf * 1000000000000) + oldfr)+
+((l * 1000000000000) + lr))+"");
+f = l;
+fr = lr;
+l = (((oldf * 1000000000000) + oldfr)+
+((l * 1000000000000) + lr)) / 1000000000000;
+lr = (((oldf * 1000000000000) + oldfr)+
+((l * 1000000000000) + lr)) % 1000000000000;
+}
 
-        printf("\n");
-        return (0);
+printf("\n");
+return (0);
 }
