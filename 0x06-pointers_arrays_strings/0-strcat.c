caf8863896f8443286c7af852a34011d01177ff3
Obokparo Ejemuta <henry.ejemuta@gmail.com>
2022-06-16 07:08:26 -0700

    Updated 0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -19,19 +19,15 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (*dest != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
-		dest++;
 	}
 
-	while (*src != '\0')
+	while (src[j] != '\0')
 	{
-		*(dest + i) = *(src + j);
-		i++;
-		j++;
-		src++;
+		dest[i++] = src[j++];
 	}
-	*(dest + j) = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
