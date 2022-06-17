423aa99d5667d30608df37d832d89de4343edef5
Obokparo Ejemuta <henry.ejemuta@gmail.com>
2022-06-16 21:06:46 -0700

    a function that changes all lowercase letters of a string to uppercase.

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev - A C function that reverses
+ * reverse_array - A C function that reverses
  * the content of an array of integers.
  *
  * @a: array to reverse
