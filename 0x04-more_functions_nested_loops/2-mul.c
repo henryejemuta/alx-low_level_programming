49d18a20c06380bd10b7b32c91e30145ce1fcc36
Obokparo Ejemuta <henry.ejemuta@gmail.com>
2022-06-17 12:23:23 -0700

    A C a function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - A C a function that multiplies two integers.
+ *
+ * @a: The first number to multiply
+ * @b: The second number to multiply
+ *
+ * Return: "a" and "b"
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
