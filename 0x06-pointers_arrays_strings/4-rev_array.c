75873c5173401fe221d01dae39ad6c8b04e032f0
Obokparo Ejemuta <henry.ejemuta@gmail.com>
2022-06-16 20:58:59 -0700

    a function that reverses the content of an array of integers.

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - A C function that reverses
+ * the content of an array of integers.
+ *
+ * @a: array to reverse
+ * @n: number of byte in array
+ */
+void reverse_array(int *a, int n)
+{
+	int temp, i;
+
+	n = n - 1;
+	for (i = 0; i <= n;)
+	{
+		temp = a[i];
+		a[i++] = a[n];
+		a[n--] = temp;
+	}
+}
