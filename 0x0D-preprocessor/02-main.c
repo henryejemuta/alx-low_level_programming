ed1aeac87a7f23179435f268b781c0778a9f2250
Obokparo Ejemuta <henry.ejemuta@gmail.com>
2022-07-04 15:43:46 -0700

    Created a C program that prints the name of the file it was compiled from, followed by a new line.

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A C Program that prints the gile it was
+ * compiled from, followed by a new line
+ *
+ * Return: 0 Alway success
+ **/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
