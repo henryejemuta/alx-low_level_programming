b9b95518d84d7979ad8c4185e9413932e133077c
Obokparo Ejemuta <henry.ejemuta@gmail.com>
2022-07-04 17:09:12 -0700

    Added documentation for struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
+#ifndef MY_DOG_STRUCT
+#define MY_DOG_STRUCT
 /**
- * dog - Object of a dog
+ * struct dog - Object of a dog
  * @name: name of the dog
  * @age: the dog age
  * @owner: name of the owner
@@ -10,3 +12,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
