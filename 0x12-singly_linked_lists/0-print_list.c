5f12eec5de2f4db1cb2983604919bfe5d2a06e80
Obokparo Ejemuta <henry.ejemuta@gmail.com>
2022-07-14 20:13:14 -0700

    Remove stdio printf from 0x12. C - Singly linked lists Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,44 @@
 #include "lists.h"
 
 /**
+ * _cprint - A C funtion that print a string
+ *
+ * @s: The string to print
+ **/
+void _cprint(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+}
+
+
+/**
+ * print_number - A C function that prints an integer.
+ *
+ * @n: integer to be printed.
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	num = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		num = -n;
+	}
+
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
+
+/**
  * print_list - A C function that prints
  * all the elements of a list_t list.
  *
@@ -15,8 +53,19 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		(!h->str) ? printf("[0] (nil)\n")
-			: printf("[%i] %s\n", h->len, h->str);
+		if (!h->str)
+		{
+			_cprint("[0] (nil)");
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('[');
+			print_number(h->len);
+			_cprint("] ");
+			_cprint(h->str);
+			_putchar('\n');
+		}
 		h = h->next;
 		len++;
 	}
