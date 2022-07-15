e1b1d1a804593080f9e741bc8008b6a0f4ce53d9
Obokparo Ejemuta <henry.ejemuta@gmail.com>
2022-07-14 23:15:37 -0700

    Updated %i to %d on 0x12. C - Singly linked lists Task 0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,10 +15,13 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		(h->str == NULL) ? printf("[0] (nil)\n")
-			: printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		len++;
+		h = h->next;
 	}
+
 	return (len);
 }
